666250ab78a1b7ecfcba718afd35bfcfa9c9aa89
Noeusser <anva_37@hotmail.com>
2026-06-18 16:39:42 -0600

    update pipeline

--- chapter_3/work_3/dags/batch_generator2.py
+++ chapter_3/work_3/dags/batch_generator2.py
@@ -8,8 +8,8 @@ import polars as pl
 from faker import Faker
 from datetime import date, datetime, timedelta
 from airflow import DAG
-from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator # type: ignore
+from airflow.operators.python import PythonOperator # type: ignore
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 
 # Configure logging.
